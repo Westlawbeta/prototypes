31a7c90f405f57e9c226bb42a4b5f56d15cafe44
JaimeHernandezTR <Jaime.Hernandez@thomsonreuters.com>
2025-08-29 12:14:10 -0600

    Add same functionality in v10 from v9

--- src/app/pages/westlaw-prototype/components/researchv10/new-research/new-research.component.ts
+++ src/app/pages/westlaw-prototype/components/researchv10/new-research/new-research.component.ts
@@ -1,28 +1,41 @@
-import { Component } from '@angular/core';
-import { SearchBarComponent } from '../../common/search-bar/search-bar.component';
-import { BookmarksComponent } from '../../bookmarks/bookmarks.component';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { SearchBarComponent } from "../../common/search-bar/search-bar.component";
+import { BookmarksComponent } from "../../bookmarks/bookmarks.component";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-new-researchv10',
-  templateUrl: './new-research.component.html',
-  styleUrls: ['./new-research.component.less']
+  selector: "app-new-researchv10",
+  templateUrl: "./new-research.component.html",
+  styleUrls: ["./new-research.component.less"],
 })
-
 export class NewResearchComponentv10 {
   constructor(private router: Router) {}
 
-  searchType: string = 'Search'; // Default value
-  href: string ='';
+  searchType: string = "AI Deep Search"; // Default value
+  href: string = "";
+  tab1: boolean = true;
+  tab2: boolean = false;
 
   ngOnInit() {
     this.href = this.router.url;
     console.log(this.router.url);
-}
+  }
 
   searchTypeChanged(event: any) {
-    console.log(event);
     this.searchType = event;
   }
 
+  //tab1 = ai deep
+  //tab2 = search
+  handleToggleTabState(tab: string) {
+    if (tab === "tab1") {
+      this.tab1 = true;
+      this.tab2 = false;
+      this.searchTypeChanged("AI Deep Search");
+    } else if (tab === "tab2") {
+      this.tab1 = false;
+      this.tab2 = true;
+      this.searchTypeChanged("Search");
+    }
+  }
 }
